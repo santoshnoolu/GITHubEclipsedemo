5a3e628c6ed3ad2f87e7b55eab678c3fad10526d
Santosh Kumar Noolu <30371667+santoshnoolu@users.noreply.github.com>
2020-07-06 13:24:54 +0530

    Update Testclass2.java

--- GITEclipseProject/src/Testclass2.java
+++ GITEclipseProject/src/Testclass2.java
@@ -3,7 +3,7 @@ public class Testclass2 {
 public static void main(String args[])
 {
 	System.out.println("B");
-System.out.println("Modified from GIT hub");
+System.out.println("Modified from GIT hub2");
 }
 
 }
